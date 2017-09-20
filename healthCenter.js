314083b3f541da94e9ff79934b85fb016df927e4
krquinton <32116802+krquinton@users.noreply.github.com>
2017-09-20 11:45:14 +0800

    Add files via upload

---
+++ healthCenter.js
@@ -0,0 +1,116 @@
+var healthCenterData = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "1",
+        "type": "Health Center",
+        "barangay": "Barangay 176",
+        "name": "Barangay 176 Health Center"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.056475,
+          14.756943
+        ]
+      }
+    },
+        {
+      "type": "Feature",
+      "properties": {
+        "id": "2",
+        "type": "Health Center",
+        "barangay": "Barangay 171",
+        "name": "Barangay 171 Health Center"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.022462,
+          14.748461
+        ]
+      }
+    },
+        {
+      "type": "Feature",
+      "properties": {
+        "id": "3",
+        "type": "Health Center",
+        "barangay": "Barangay 177",
+        "name": "Cielito Health Center"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.049623,
+          14.746905
+        ]
+      }
+    },
+        {
+      "type": "Feature",
+      "properties": {
+        "id": "4",
+        "type": "Health Center",
+        "barangay": "Barangay 168",
+        "name": "Deparo 176 Health Center"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.964662,
+          14.648598
+        ]
+      }
+    },
+        {
+      "type": "Feature",
+      "properties": {
+        "id": "5",
+        "type": "Health Center",
+        "barangay": "Barangay 14",
+        "name": "Barangay 14 Health Center"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.964662,
+          14.648598
+        ]
+      }
+    },
+        {
+      "type": "Feature",
+      "properties": {
+        "id": "6",
+        "type": "Health Center",
+        "barangay": "Barangay 174",
+        "name": "Barangay 174 Health Center"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.048142,
+          14.762178
+        ]
+      }
+    },
+        {
+      "type": "Feature",
+      "properties": {
+        "id": "7",
+        "type": "Health Center",
+        "barangay": "Barangay 178",
+        "name": "Barangay 178 Health Center"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.056442,
+          14.756937
+        ]
+      }
+    }]}
